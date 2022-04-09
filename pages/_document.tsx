65261d4dfe84061f9ba4d22bd4a60e3762e67d64
sscarry2 <earth.narongsak@gmail.com>
2022-04-09 17:33:36 +0700

    add theme and fonts

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -35,7 +35,7 @@ class MyDocument extends Document {
           <link rel="manifest" href="/manifest.json" />
           <link rel="apple-touch-icon" href="/icon.png"></link>
           <link
-            href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap"
+            href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap"
             rel="stylesheet"></link>
           <meta name="theme-color" content="#fff" />
         </Head>
